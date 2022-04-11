c0993955540c9c6a4f8ad9153391b2b8ced6c206
ruleBased848 <rkdfowns@naver.com>
2022-04-12 03:01:08 +0900

    Use ServerSocket

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.net.*;
+import java.io.*;
+
+class Main
+{
+    public static void main(String[] args) throws Exception
+    {
+        var ss = new ServerSocket(8080);
+
+        String str = "abc\n";
+        var buf = str.getBytes();
+
+        while (true)
+        {
+            var socket = ss.accept();
+            var out = socket.getOutputStream();
+            out.write(buf);
+            socket.close();
+        }
+    }
+}
