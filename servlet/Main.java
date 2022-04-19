e4db771ed034ea8e081dadec6ffab7d7633ab3c4
ruleBased848 <rkdfowns@naver.com>
2022-04-19 16:36:06 +0900

    Use WebServlet annotation

--- servlet/Main.java
+++ servlet/Main.java
@@ -3,7 +3,9 @@ package servlet;
 import java.io.*;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.WebServlet;
 
+@WebServlet("/main")
 public class Main extends HttpServlet
 {
     public void service(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
