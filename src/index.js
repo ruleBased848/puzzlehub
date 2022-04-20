963dc2ac6ed57beb47f5de789b32c3dd3089a380
ruleBased848 <rkdfowns@naver.com>
2022-04-20 18:02:00 +0900

    Use createRoot

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { action, observable } from 'mobx';
 import { observer } from "mobx-react";
 import './index.css';
@@ -109,7 +109,4 @@ function Game() {
 
 // ========================================
 
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
+createRoot(document.getElementById('root')).render(<Game />);
