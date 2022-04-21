b0580944be86c0b6e4e29b57b9f1981794cccd26
ruleBased848 <rkdfowns@naver.com>
2022-04-21 19:06:50 +0900

    Use fetch

--- src/components/Submit.js
+++ src/components/Submit.js
@@ -1,5 +1,11 @@
 function Submit() {
-  return <div className="submit-background">Submit</div>;
+  const clickHandler = () => {
+    fetch("/main")
+      .then((response) => response.text())
+      .then((text) => alert(text));
+  };
+
+  return <div className="submit-background" onClick={clickHandler}>Submit</div>;
 }
 
 export default Submit;
