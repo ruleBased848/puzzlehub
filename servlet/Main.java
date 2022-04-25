ecb47109754988ee3992e99e9343e627106420b7
ruleBased848 <rkdfowns@naver.com>
2022-04-25 18:55:21 +0900

    Add isValidRow method

--- servlet/Main.java
+++ servlet/Main.java
@@ -29,6 +29,25 @@ public class Main extends HttpServlet
         return areUnique(al);
     }
 
+    private boolean isValidRow(String[][] board, int row)
+    {
+        int box = row / 3 * 3;
+        int cell = row % 3 * 3;
+        var al = new ArrayList<String>();
+        for (int i = 0; i < 3; ++i)
+        {
+            for (int j = 0; j < 3; ++j)
+            {
+                var str = board[box + i][cell + j];
+                if (!str.equals(""))
+                {
+                    al.add(str);
+                }
+            }
+        }
+        return areUnique(al);
+    }
+
     private boolean areUnique(ArrayList<String> al)
     {
         Collections.sort(al);
