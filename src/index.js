e2c8daca8da954fb3b94d0ef7098218088b07049
ruleBased848 <rkdfowns@naver.com>
2022-04-26 21:43:00 +0900

    Use React Router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Game from './Game';
 import './index.css';
 
-createRoot(document.getElementById('root')).render(<Game />);
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Game />} />
+    </Routes>
+  </BrowserRouter>
+);
