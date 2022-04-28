f1921a9259553ac7c991768bcd581f5c694d0c77
ruleBased848 <rkdfowns@naver.com>
2022-04-28 14:08:10 +0900

    Add SignUp page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Game from './Game';
+import SignUp from './SignUp';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Game />} />
+        <Route path="signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
