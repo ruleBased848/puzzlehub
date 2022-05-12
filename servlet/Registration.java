87f09d56d49e3ebb9cc4b0cf4586352451846741
ruleBased848 <rkdfowns@naver.com>
2022-05-12 23:10:13 +0900

    Refactor Registration class

--- servlet/Registration.java
+++ servlet/Registration.java
@@ -2,6 +2,7 @@ package servlet;
 
 import java.io.*;
 import java.sql.*;
+import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.WebServlet;
 import org.json.simple.*;
@@ -10,37 +11,66 @@ import org.json.simple.parser.*;
 @WebServlet("/registration")
 public class Registration extends HttpServlet
 {
-    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException
+    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+    {
+        try
+        {
+            service_(request, response);
+        }
+        catch (ClassNotFoundException | SQLException e)
+        {
+            throw new ServletException(e);
+        }
+    }
+
+    private void service_(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ClassNotFoundException, SQLException
     {
         var in = request.getReader();
         var json = in.readLine();
         in.close();
 
-        var out = response.getWriter();
+        JSONObject jo = null;
         try
         {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            var conn = DriverManager.getConnection(
-                "jdbc:mysql://" +
-                System.getProperty("DBSERVER") +
-                "/" +
-                System.getProperty("DATABASE") +
-                "?user=" +
-                System.getProperty("USER") +
-                "&password=" +
-                System.getProperty("PASSWORD")
-            );
-            var pStmt = conn.prepareStatement("INSERT INTO users VALUES (?,?)");
-            var jo = (JSONObject)(new JSONParser().parse(json));
-            pStmt.setString(1, (String)jo.get("username"));
-            pStmt.setString(2, (String)jo.get("password"));
-            pStmt.executeUpdate();
-            out.println("{\"ok\":true}");
+            jo = (JSONObject)(new JSONParser().parse(json));
         }
-        catch (Exception e)
+        catch (ParseException e)
         {
+            var out = response.getWriter();
             out.println("{\"ok\":false}");
-            System.out.println(e);
+            out.close();
+            return;
         }
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        var conn = DriverManager.getConnection(
+            "jdbc:mysql://" +
+            System.getProperty("DBSERVER") +
+            "/" +
+            System.getProperty("DATABASE") +
+            "?user=" +
+            System.getProperty("USER") +
+            "&password=" +
+            System.getProperty("PASSWORD")
+        );
+        var pStmt = conn.prepareStatement("INSERT INTO users VALUES (?,?)");
+        pStmt.setString(1, (String)jo.get("username"));
+        pStmt.setString(2, (String)jo.get("password"));
+        try
+        {
+            pStmt.executeUpdate();
+        }
+        catch (SQLException e)
+        {
+            var out = response.getWriter();
+            out.println("{\"ok\":false}");
+            out.close();
+            return;
+        }
+
+        var out = response.getWriter();
+        out.println("{\"ok\":true}");
+        out.close();
     }
 }
