e57294391527453d8ac5f4ef1466b78459fd72c6
ruleBased848 <rkdfowns@naver.com>
2022-05-12 15:15:41 +0900

    Refactor Authentication class

--- servlet/Authentication.java
+++ servlet/Authentication.java
@@ -11,86 +11,68 @@ import org.json.simple.parser.*;
 @WebServlet("/authentication")
 public class Authentication extends HttpServlet
 {
-    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException
+    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+    {
+        try
+        {
+            service_(request, response);
+        }
+        catch (ClassNotFoundException | SQLException e)
+        {
+            throw new ServletException(e);
+        }
+    }
+
+    private void service_(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ClassNotFoundException, SQLException
     {
         var in = request.getReader();
         var json = in.readLine();
         in.close();
 
-        var out = response.getWriter();
+        JSONObject jo = null;
         try
         {
-            var jo = (JSONObject)(new JSONParser().parse(json));
-            var username = (String)jo.get("username");
-            var password = (String)jo.get("password");
-
-            loadDriver();
-            var conn = getConnection();
-            var pStmt = prepareStatement(conn, "SELECT password FROM users WHERE username = ?");
-            pStmt.setString(1, username);
-            var result = pStmt.executeQuery();
-            var ok = false;
-            if (result.next())
-            {
-                var password_ = result.getString("password");
-                if (password_.equals(password))
-                {
-                    ok = true;
-                    response.addCookie(new Cookie("username", username));
-                    response.addCookie(new Cookie("password", password));
-                }
-            }
-            out.println("{\"ok\":" + ok + "}");
+            jo = (JSONObject)(new JSONParser().parse(json));
         }
-        catch (Exception e)
+        catch (ParseException e)
         {
+            var out = response.getWriter();
             out.println("{\"ok\":false}");
-            System.out.println(e);
+            out.close();
+            return;
         }
-    }
+        var username = (String)jo.get("username");
+        var password = (String)jo.get("password");
 
-    private void loadDriver() throws ServletException
-    {
-        try
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        var conn = DriverManager.getConnection(
+            "jdbc:mysql://" +
+            System.getProperty("DBSERVER") +
+            "/" +
+            System.getProperty("DATABASE") +
+            "?user=" +
+            System.getProperty("USER") +
+            "&password=" +
+            System.getProperty("PASSWORD")
+        );
+        var pStmt = conn.prepareStatement("SELECT password FROM users WHERE username = ?");
+        pStmt.setString(1, username);
+        var result = pStmt.executeQuery();
+        var ok = false;
+        if (result.next())
         {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            var password_ = result.getString("password");
+            if (password_.equals(password))
+            {
+                ok = true;
+                response.addCookie(new Cookie("username", username));
+                response.addCookie(new Cookie("password", password));
+            }
         }
-        catch (ClassNotFoundException e)
-        {
-            throw new ServletException(e);
-        }
-    }
 
-    private Connection getConnection() throws ServletException
-    {
-        try
-        {
-            return DriverManager.getConnection(
-                "jdbc:mysql://" +
-                System.getProperty("DBSERVER") +
-                "/" +
-                System.getProperty("DATABASE") +
-                "?user=" +
-                System.getProperty("USER") +
-                "&password=" +
-                System.getProperty("PASSWORD")
-            );
-        }
-        catch (SQLException e)
-        {
-            throw new ServletException(e);
-        }
-    }
-
-    private PreparedStatement prepareStatement(Connection conn, String sql) throws ServletException
-    {
-        try
-        {
-            return conn.prepareStatement(sql);
-        }
-        catch (SQLException e)
-        {
-            throw new ServletException(e);
-        }
+        var out = response.getWriter();
+        out.println("{\"ok\":" + ok + "}");
+        out.close();
     }
 }
