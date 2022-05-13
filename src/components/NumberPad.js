d96e96b0b11abd95c318b099ccc05e562b45b046
ruleBased848 <rkdfowns@naver.com>
2022-05-14 03:42:36 +0900

    Refactor NumberPad component

--- src/components/NumberPad.js
+++ src/components/NumberPad.js
@@ -1,26 +1,18 @@
 import { updateNumbers, selectedCell } from '../states';
 import './NumberPad.css';
 
-function NumberPadRow({ labels }) {
-  return (
-    <div className="numberpad-row">
-      {Array(3).fill().map((e, i) => <div key={i} className="numberpad-cell" onClick={() => updateNumbers(selectedCell, labels[i])}>{labels[i]}</div>)}
-    </div>
-  );
-}
-
-function NumberPadDelete() {
-  return <div className="numberpad-delete" onClick={() => updateNumbers(selectedCell, "")}>X</div>;
-}
-
 function NumberPad() {
+  const clickHandler = (i) => () => updateNumbers(selectedCell, "" + (i + 1));
+  const deleteHandler = () => updateNumbers(selectedCell, "");
+
   return (
-    <div className="numberpad-background">
-      <NumberPadRow labels={["1", "2", "3"]} />
-      <NumberPadRow labels={["4", "5", "6"]} />
-      <NumberPadRow labels={["7", "8", "9"]} />
-      <NumberPadDelete />
-    </div>
+    <svg className="sudoku-numberpad" viewBox="0 0 1055 110" preserveAspectRatio="none">
+      <rect width="1055" height="110" fill="black" />
+      {Array(9).fill().map((e, i) => <rect key={i} className="button" x={5 + 105 * i} y="5" width="100" height="100" fill="white" onClick={clickHandler(i)} />)}
+      {Array(9).fill().map((e, i) => <text key={i} x={55 + 105 * i} y="75" textAnchor="middle" fontSize="58" fill="blue" onClick={clickHandler(i)}>{i + 1}</text>)}
+      <rect className="button" x="950" y="5" width="100" height="100" fill="white" onClick={deleteHandler} />
+      <text x="1000" y="75" textAnchor="middle" fontSize="58" fill="red" onClick={deleteHandler}>&times;</text>
+    </svg>
   );
 }
 
