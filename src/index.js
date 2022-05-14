5e56952d146941dc507cbab808237d2b008ffab9
ruleBased848 <rkdfowns@naver.com>
2022-05-14 12:22:31 +0900

    Update src/index.js
    로그인 상태 관리 기능 추가

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+import { signIn } from './states';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(<App />);
+fetch("/cookieauthentication")
+  .then((response) => response.json())
+  .then((json) => signIn.set(json.ok));
