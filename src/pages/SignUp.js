97e8a1770a7e5e54eb987a599225614e12fbb335
ruleBased848 <rkdfowns@naver.com>
2022-05-17 14:41:34 +0900

    Update SignUp page

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -3,9 +3,7 @@ import SignUpForm from '../components/SignUpForm';
 function SignUp() {
   return (
     <div>
-      <div>
-        <SignUpForm />
-      </div>
+      <SignUpForm />
     </div>
   );
 }
