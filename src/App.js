7d8807640342c7456c983f7942914534b85af70f
ruleBased848 <rkdfowns@naver.com>
2022-05-22 20:29:33 +0900

    Add Upload page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Game from './pages/Game';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
+import Upload from './pages/Upload';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="index.html" element={<Game />} />
         <Route path="signup" element={<SignUp />} />
         <Route path="signin" element={<SignIn />} />
+        <Route path="upload" element={<Upload />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Upload.js
@@ -0,0 +1,115 @@
+import { observer } from "mobx-react";
+import { numbers, selectedCell, resetSudoku, signIn } from '../states';
+import { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Board from '../components/Board';
+import NumberPad from '../components/NumberPad';
+import styles from './Upload.module.css';
+
+const Upload = observer(
+  () => {
+    const username = useRef(null);
+    const password = useRef(null);
+    const remember = useRef(null);
+    const navigate = useNavigate();
+
+    const submitHandler = () => {
+      fetch("/members/main", {
+        method: "POST",
+        body: JSON.stringify(numbers),
+      })
+        .then((response) => response.json())
+        .then((json) => {
+          if (json.ok)
+          {
+            resetSudoku();
+            navigate("/");
+          }
+          else if (json.reason === "invalid puzzle")
+          {
+            alert(`Only puzzles with one answer can be submitted.\nThere are ${json.number === 1000 ? "999+" : json.number} answers to this puzzle.`);
+          }
+          else if (json.reason === "not authenticated")
+          {
+            signIn.set(false);
+          }
+        });
+    };
+
+    const cancelHandler = () => {
+      resetSudoku();
+      navigate("/");
+    };
+
+    const signinHandler = () => {
+      if (username.current.value === "")
+      {
+        alert("The username is required.");
+      }
+      else if (password.current.value === "")
+      {
+        alert("The password is required.");
+      }
+      else
+      {
+        fetch("/authentication", {
+          method: "POST",
+          body: JSON.stringify({
+            username: username.current.value,
+            password: password.current.value,
+            remember: remember.current.checked,
+          }),
+        })
+          .then((response) => response.json())
+          .then((json) => {
+            if (json.ok)
+            {
+              signIn.set(true);
+            }
+            else
+            {
+              alert(`Authentication failed.`);
+            }
+          });
+      }
+    };
+
+    return signIn.get() ? (
+      <div className={styles.background}>
+        <div className={styles.container}>
+          <div>
+            <Board numbers={numbers} selectedCell={selectedCell} />
+          </div>
+          <div>
+            <NumberPad />
+          </div>
+          <button className={styles.submitbutton} type="button" onClick={submitHandler}>Submit</button>
+          <button className={styles.cancelbutton} type="button" onClick={cancelHandler}>Cancel</button>
+        </div>
+      </div>
+    ) : (
+      <div>
+        <form className={styles.signin}>
+          <div className={styles.title}>Sign in</div>
+          <div>
+            <label className={styles.mainlabel} htmlFor="username">User name</label>
+            <input type="text" id="username" className={styles.maininput} ref={username} />
+          </div>
+          <div>
+            <label className={styles.mainlabel} htmlFor="password">Password</label>
+            <input type="password" id="password" className={styles.maininput} ref={password} />
+          </div>
+          <div className={styles.check}>
+            <input type="checkbox" id="remember" className={styles.checkboxinput} ref={remember}/>
+            <label className={styles.checkboxlabel} htmlFor="remember">Remember me</label>
+          </div>
+          <div>
+            <button className={styles.signinbutton} type="button" onClick={signinHandler}>Sign In</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+);
+
+export default Upload;
