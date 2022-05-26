77d2c5946156a38e47daeac170d52363a19d96a3
ruleBased848 <rkdfowns@naver.com>
2022-05-27 00:31:31 +0900

    Update Submit component

--- src/components/Submit.js
+++ src/components/Submit.js
@@ -1,17 +1,7 @@
-import { numbers } from '../states';
 import styles from './Submit.module.css';
 
 function Submit() {
-  const clickHandler = () => {
-    fetch("/members/main", {
-      method: "POST",
-      body: JSON.stringify(numbers),
-    })
-      .then((response) => response.text())
-      .then((text) => alert(text));
-  };
-
-  return <div className={styles.background} onClick={clickHandler}>Submit</div>;
+  return <div className={styles.background}>Submit</div>;
 }
 
 export default Submit;
