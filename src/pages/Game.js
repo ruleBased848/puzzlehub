04281ca9180573b5b8444a000dc6a60dfc813e62
ruleBased848 <rkdfowns@naver.com>
2022-06-01 03:30:46 +0900

    Update Game page

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,32 +1,71 @@
 import { observer } from "mobx-react";
-import { sudokuState, signIn } from '../states';
+import { signIn, pageControlState, changeSearchState, changeHamburgerState, closeAll } from '../states';
+import { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
-import Board from '../components/Board';
-import Submit from '../components/Submit';
+import styles from './Game.module.css';
 
-const Game = observer(
-  () => {
-    const linksSignInFalse = (
-      <div>
-        <Link to="/signup">Sign up</Link>
-        <Link to="/signin">Sign in</Link>
-      </div>
-    );
+const Header = observer(() => {
+  const search = useRef(null);
 
-    return (
-      <div>
-        {signIn.get() ? <div /> : linksSignInFalse}
-        <div className="game">
-          <div className="game-board">
-            <Board numbers={sudokuState.numbers} selectedCell={sudokuState.selectedCell} />
-          </div>
-          <div className="submit">
-            <Submit />
-          </div>
-        </div>
+  useEffect(() => {
+    if (pageControlState.searchOpen)
+    {
+      search.current.focus();
+    }
+  },
+  // eslint-disable-next-line
+  [pageControlState.searchOpen]);
+
+  const searchClickHandler = (e) => {
+    e.stopPropagation();
+    changeSearchState();
+  };
+
+  const hamburgerClickHandler = (e) => {
+    e.stopPropagation();
+    changeHamburgerState();
+  };
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.logocontainer}>
+        <img className={styles.image} src="/logo40.png" alt="Puzzle hub" />
       </div>
-    );
-  }
-);
+      <div className={styles.searchcontainer}>
+        <button className={styles.button} type="button" onClick={searchClickHandler}>
+          <img className={styles.image} src="/assets/search.png" alt="search" />
+        </button>
+        <input type="search" className={`${styles.search} ${pageControlState.searchOpen ? styles.visible : styles.invisible}`} ref={search} onClick={(e) => e.stopPropagation()} />
+      </div>
+      <div className={styles.hamburgercontainer}>
+        <button className={styles.button} type="button" onClick={hamburgerClickHandler}>
+          <img className={styles.image} src="/assets/hamburger.png" alt="menu" />
+        </button>
+        <nav className={`${styles.nav} ${pageControlState.hamburgerOpen ? styles.visible : styles.invisible}`} onClick={(e) => e.stopPropagation()}>
+          {signIn.get() ? <ul className={styles.ul} /> : (
+            <ul className={styles.ul}>
+              <li>
+                <Link to="/signup">Sign up</Link>
+              </li>
+              <li>
+                <Link to="/signin">Sign in</Link>
+              </li>
+            </ul>
+          )}
+        </nav>
+      </div>
+    </header>
+  );
+});
+
+function Game() {
+  return (
+    <div className={styles.background} onClick={closeAll}>
+      <Header />
+      <main className={styles.main}>
+      </main>
+    </div>
+  );
+}
 
 export default Game;
