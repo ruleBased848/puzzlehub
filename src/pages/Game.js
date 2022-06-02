b5e535638470e4f80a4f04d999eabac50dd0165a
ruleBased848 <rkdfowns@naver.com>
2022-06-02 14:48:46 +0900

    Update Game page

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,5 +1,5 @@
 import { observer } from "mobx-react";
-import { signIn, pageControlState, changeSearchState, changeHamburgerState, closeAll } from '../states';
+import { signIn, pageControlState, changeSearchState, changeHamburgerState, closeAll, updatePage } from '../states';
 import { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import Async from "react-async";
@@ -76,22 +76,49 @@ const Main = observer(() => {
       <Async promiseFn={fetchPuzzles} itemNum={itemNum} page={pageControlState.page}>
         <Async.Pending>Loading...</Async.Pending>
         <Async.Fulfilled>
-          {(data) => (
-            <div className={styles.puzzledisplay}>
-              <div className={styles.puzzlelist}>
-                {data.puzzles.map((e, i) => (
-                  <div key={i} className={styles.puzzleinfo}>
-                    <div className={styles.boardcontainer}>
-                      <Board numbers={[e.content]} selectedCell={[0, -1]} />
-                      <div className={styles.meta}>
-                        {e.username}<br />{e.createdAt}
+          {(data) => {
+            const firstPageNumber = Math.floor((pageControlState.page - 1) / 10) * 10 + 1;
+            const lastPageNumber = Math.max(Math.ceil(data.number / itemNum), pageControlState.page);
+            const pageNumberCount = Math.min(lastPageNumber - firstPageNumber + 1, 10);
+
+            return (
+              <div className={styles.puzzledisplay}>
+                <div className={styles.puzzlelist}>
+                  {data.puzzles.map((e, i) => (
+                    <div key={i} className={styles.puzzleinfo}>
+                      <div className={styles.boardcontainer}>
+                        <Board numbers={[e.content]} selectedCell={[0, -1]} />
+                        <div className={styles.meta}>
+                          {e.username}<br />{e.createdAt}
+                        </div>
                       </div>
                     </div>
+                  ))}
+                </div>
+                <div className={styles.pagination}>
+                  {firstPageNumber === 1 ? (
+                    <div className={`${styles.prev} ${styles.deactivated}`}>PREV</div>
+                  ) : (
+                    <div className={`${styles.prev} ${styles.activated}`} onClick={() => updatePage(firstPageNumber - 1)}>PREV</div>
+                  )}
+                  <div className={styles.pagenumbercontainer}>
+                    {Array(pageNumberCount).fill().map((e, i) => (
+                      i + firstPageNumber === pageControlState.page ? (
+                        <span key={i} className={`${styles.pagenumber} ${styles.selected}`}>{i + firstPageNumber}</span>
+                      ) : (
+                        <span key={i} className={`${styles.pagenumber} ${styles.unselected}`} onClick={() => updatePage(i + firstPageNumber)}>{i + firstPageNumber}</span>
+                      )
+                    ))}
                   </div>
-                ))}
+                  {lastPageNumber - firstPageNumber >= 10 ? (
+                    <div className={`${styles.next} ${styles.activated}`} onClick={() => updatePage(firstPageNumber + 10)}>NEXT</div>
+                  ) : (
+                    <div className={`${styles.next} ${styles.deactivated}`}>NEXT</div>
+                  )}
+                </div>
               </div>
-            </div>
-          )}
+            );
+          }}
         </Async.Fulfilled>
       </Async>
     </main>
