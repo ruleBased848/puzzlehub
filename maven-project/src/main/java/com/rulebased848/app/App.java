5cc5986555e9bd102b83cf620973f54ece71ff6c
ruleBased848 <rkdfowns@naver.com>
2022-06-11 21:53:51 +0900

    Use Maven

---
+++ maven-project/src/main/java/com/rulebased848/app/App.java
@@ -0,0 +1,7 @@
+package com.rulebased848.app;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ maven-project/src/test/java/com/rulebased848/app/AppTest.java
@@ -0,0 +1,11 @@
+package com.rulebased848.app;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
