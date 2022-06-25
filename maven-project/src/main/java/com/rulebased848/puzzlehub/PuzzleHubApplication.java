77981e86b81473f6d0eab747c4689509318275eb
ruleBased848 <rkdfowns@naver.com>
2022-06-26 03:53:27 +0900

    Make the spring boot application class

---
+++ maven-project/src/main/java/com/rulebased848/puzzlehub/PuzzleHubApplication.java
@@ -0,0 +1,11 @@
+package com.rulebased848.puzzlehub;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class PuzzleHubApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PuzzleHubApplication.class, args);
+    }
+}
