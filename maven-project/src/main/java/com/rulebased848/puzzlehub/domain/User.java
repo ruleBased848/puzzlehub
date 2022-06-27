3a48d70c6fff6f6adb7c978837c3ca3f0a210f0c
ruleBased848 <rkdfowns@naver.com>
2022-06-28 07:40:15 +0900

    Add User entity

---
+++ maven-project/src/main/java/com/rulebased848/puzzlehub/domain/User.java
@@ -0,0 +1,26 @@
+package com.rulebased848.puzzlehub.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
