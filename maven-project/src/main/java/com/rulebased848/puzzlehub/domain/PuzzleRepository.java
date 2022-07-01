2114691a5bf9c9680a48709381dd8ae040d864a5
ruleBased848 <rkdfowns@naver.com>
2022-07-01 17:50:02 +0900

    Update PuzzleRepository interface

--- maven-project/src/main/java/com/rulebased848/puzzlehub/domain/PuzzleRepository.java
+++ maven-project/src/main/java/com/rulebased848/puzzlehub/domain/PuzzleRepository.java
@@ -1,6 +1,9 @@
 package com.rulebased848.puzzlehub.domain;
 
 import org.springframework.data.repository.*;
+import org.springframework.data.jpa.repository.*;
 
 public interface PuzzleRepository extends CrudRepository<Puzzle, Integer> {
+    @Query("select count(p) from Puzzle p")
+    long getCountOfPuzzles();
 }
