01a7358b7e8250149ee1d5386768fe19a9bc962a
ruleBased848 <rkdfowns@naver.com>
2022-07-10 22:45:10 +0900

    Update UserRepository interface

--- maven-project/src/main/java/com/rulebased848/puzzlehub/domain/UserRepository.java
+++ maven-project/src/main/java/com/rulebased848/puzzlehub/domain/UserRepository.java
@@ -1,6 +1,8 @@
 package com.rulebased848.puzzlehub.domain;
 
+import java.util.*;
 import org.springframework.data.repository.*;
 
 public interface UserRepository extends CrudRepository<User, String> {
+    Optional<User> findByUsername(String username);
 }
