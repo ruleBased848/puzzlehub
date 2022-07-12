a266cd4d2a5d0ae034b59f4e2f4735b4166ce512
ruleBased848 <rkdfowns@naver.com>
2022-07-12 13:33:35 +0900

    Update SecurityConfig configuration

--- maven-project/src/main/java/com/rulebased848/puzzlehub/SecurityConfig.java
+++ maven-project/src/main/java/com/rulebased848/puzzlehub/SecurityConfig.java
@@ -1,13 +1,27 @@
 package com.rulebased848.puzzlehub;
 
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.context.annotation.*;
 import org.springframework.security.web.*;
 import org.springframework.security.config.annotation.web.builders.*;
+import org.springframework.security.config.annotation.authentication.builders.*;
+import org.springframework.security.crypto.bcrypt.*;
+import com.rulebased848.puzzlehub.service.*;
 
 @Configuration
 public class SecurityConfig {
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.build();
+    }
+
+    @Autowired
+    public void configureAmb(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .userDetailsService(userDetailsService)
+            .passwordEncoder(new BCryptPasswordEncoder());
     }
 }
